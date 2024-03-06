345417cab821528754db26563c92696a995d3fcb
siddiquiaffan <siddiquiaffan201@gmail.com>
2024-03-07 00:32:41 +0530

    push to github

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "prisma/generated/client";
 
 declare global {
   // allow global `var` declarations
